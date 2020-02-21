8655f8e280871adaba0f478a3369ac5e1e607e17
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-02-20 19:41:51 -0500

    MVP

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,22 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
+import 'mutationobserver-shim';
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test ('first name in form', () => {
+  const {getByText} = render(<App />);
+  const firstName = getByText(/first name*/i);
+  expect(firstName).toBeInTheDocument();
+});
+
+test ('contains last name in form', () => {
+  const {getByText} = render(<App />);
+  const lastName = getByText(/last name*/i);
+  expect(lastName).toBeInTheDocument();
+});
+
+
